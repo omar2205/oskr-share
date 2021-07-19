75f837efe8f886ac568972f99c354f2226362431
omar2205 <omar2205a@yahoo.com>
2021-07-19 17:09:09 +0200

    fix history issue
    back after share will keep going back
    to the same share link

--- src/components/Share.vue
+++ src/components/Share.vue
@@ -1,14 +1,14 @@
 <template>
-  <van-row justify="center" align="center">
-    <van-col span="12">
-      <h3>Share</h3>
-      <div class="center">
-        <van-loading />
-      </div>
-      <br />
-      <pre>{{ JSON.stringify(props.query, null, 2) }}</pre>
-    </van-col>
-  </van-row>
+    <van-row justify="center" align="center">
+        <van-col span="12">
+            <h3>Share</h3>
+            <div class="center">
+                <van-loading />
+            </div>
+            <br />
+            <h4 v-if="props.query.title">{{ props.query.title }}</h4>
+        </van-col>
+    </van-row>
 </template>
 
 <script setup>
@@ -19,39 +19,47 @@ import { Toast, Loading } from 'vant'
 import db from '../db.js'
 
 const props = defineProps({
-  query: Object,
+    query: Object,
 })
 
 const router = useRouter()
 
 onMounted(async () => {
-  /* const q = query(collection(db, 'oskr-share'), where('text', '==', props.query.text)) */
-  const q = query(collection(db, 'oskr-share'), where('text', '==', props.query.text))
-  const check = await getDocs(q)
-  if (check.docs.length > 0) {
-    Toast({message: 'Already exists', icon: 'good-job-o'})
-    return
-  }
-  try {
-    const docRef = await addDoc(collection(db, 'oskr-share'), {
-      text: props.query.text || 'null',
-      title: props.query.title || 'null',
-      url: props.query.url || 'null',
-      created_at: new Date(),
-    })
-    router.push('/')
-  } catch (err) {
-    console.log(err)
-    Toast.fail('err')
-  }
+    window.location.replace('/')
+    const q = query(
+        collection(db, 'oskr-share'),
+        where('text', '==', props.query.text)
+    )
+    const check = await getDocs(q)
+    if (check.docs.length > 0) {
+        Toast({ message: 'Already exists', icon: 'good-job-o' })
+        return
+    }
+    try {
+        const docRef = await addDoc(collection(db, 'oskr-share'), {
+            text: props.query.text || 'null',
+            title: props.query.title || 'null',
+            url: props.query.url || 'null',
+            created_at: new Date(),
+        })
+        router.push('/')
+    } catch (err) {
+        console.log(err)
+        Toast.fail('err')
+        router.push('/')
+    }
 })
 </script>
 
 <style>
 .center {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 </style>
-
+<style scoped>
+pre {
+    white-space: pre-wrap;
+}
+</style>>
